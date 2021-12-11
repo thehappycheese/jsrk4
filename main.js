223fa4316c4f513f8d82277a9c1c0ba523d43278
thehappycheese <the.nicholas.archer@gmail.com>
2021-12-12 00:31:54 +0800

    got some drawing happening

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
 
 
+function benchmark(f, name, iterations=20){
+    
+    let times = []
+    for(let i = 0;i < iterations;i++){
+        let s = performance.now()
+        f()
+        let e = performance.now()
+        times.push(e-s)
+    }
+    console.log(`=== BENCHMARK: ${name} ===`)
+    console.log(times)
+    console.log(times.reduce((acc,item)=>acc+item)/times.length)
+}
+
+
 let NUMNODES = 500;
 
 let intitial_positions = Array.from(new Array(NUMNODES), (item, index) => [
@@ -8,46 +23,62 @@ let intitial_positions = Array.from(new Array(NUMNODES), (item, index) => [
 ]);
 
 let position = v32.from_rows(intitial_positions);
-let mass     = v32.from_column(new Array(NUMNODES).fill(1));
+//let mass     = v32.from_column(new Array(NUMNODES).fill(1));
 
 let velocity = v32.from_rows(new Array(NUMNODES).fill([0,0]));
 
 
-let edges                = Array.from(Array(NUMNODES-1), (_, index) => [index, index+1])
-let edge_rest_length     = v32.from_column(new Array(NUMNODES-1).fill(5))
-let edge_spring_constant = v32.from_column(new Array(NUMNODES-1).fill(0.1))
-let edge_delta           = position.arg_delta_store(edges, v32.from_zeros(NUMNODES-1, 2));
-let edge_length          = edge_delta.magnitude_store(v32.from_zeros(NUMNODES-1, 1))
+let edges                = Uint32Array.from(new Uint32Array((NUMNODES-1)*2), (_, index) => Math.floor(index/2) + 1*(Math.floor(index/2) % 2));
+let edge_rest_length     = v32.from_column(new Array(NUMNODES-1).fill(5));
+let edge_spring_constant = v32.from_column(new Array(NUMNODES-1).fill(0.1));
+let edge_vector          = position.arg_delta_store(edges, v32.from_zeros(NUMNODES-1, 2));
+let edge_length          = edge_vector.magnitude_store(v32.from_zeros(NUMNODES-1, 1));
 
+let edge_force_magnitude = edge_rest_length.sub_store(edge_length, v32.from_zeros(NUMNODES-1,1)).mul_self(edge_spring_constant);
 
-let edge_force_magnitude = edge_rest_length.sub_store(edge_length, v32.from_zeros(NUMNODES-1,1)).mul_self(edge_spring_constant)
-
-let edge_unit
-
-
-
-// let position_deltas = position.diff_store(v32.from_zeros(NUMNODES-1, 1))
-
-
-let position2 = v32.from_rows(intitial_positions);
-
-
-position2.scalar_mul_store(2, position2);
+// points to same object as edge vector
+let edge_force            = edge_vector.div_column_self(edge_length).replace_nan_self().mul_column_self(edge_force_magnitude);
 
 
 
 
 
-
-let position_diff = position.sub_store(position2, v32.from_zeros(NUMNODES, 2))
+//let position_diff = position.sub_store(position2, v32.from_zeros(NUMNODES, 2))
 //let magnitude = position_diff.magnitude_store(v32.from_zeros(NUMNODES, 1))
 
-
+let canvas = document.querySelector("canvas")
+let ctx = canvas.getContext('2d');
 run(performance.now())
 function run(time){
-    
-    
-
-
+    //simulate()
+    draw()
     requestAnimationFrame(run)
+}
+
+
+function simulate(){
+    position.arg_delta_store(edges, edge_vector);
+    edge_vector.magnitude_store(edge_length);
+
+    edge_rest_length.sub_store(edge_length, edge_force_magnitude).mul_self(edge_spring_constant);
+
+    edge_vector.div_column_self(edge_length).replace_nan_self().mul_column_self(edge_force_magnitude);
+
+    
+    velocity.add_self(edge_vector);
+    position.add_self(velocity);
+    velocity.scalar_mul_self(0.98);
+}
+
+
+
+function draw(){
+    let rows = position.iter_rows();
+    ctx.beginPath()
+    ctx.moveTo(...rows.next().value);
+    for(let coords of rows){
+        ctx.lineTo(...coords);
+    }
+    ctx.stroke();
+
 }
--- v32.js
+++ v32.js
@@ -1,4 +1,16 @@
-// numpy defaults to row major ordering, so we will go with that
+// numpy defaults to row major ordering in memory layout... i dont know what that means; we have opted for
+//  0,  1,  2
+//  3,  4,  5
+//  6,  7,  8
+//  9, 10, 11
+// 12, 13, 14
+// 15, 16, 17
+// ....
+// Each vector 
+
+
+// note: calling mul_column_self(_.invert_self()) is about half as fast
+
 class v32{
     data;
     rows;
@@ -74,12 +86,11 @@ class v32{
      */
     arg_delta_store(index_pairs, store){
         let result_index = 0;
-        for(let [index_a, index_b] of index_pairs){
-            index_a *= this.columns;
-            index_b *= this.columns;
-            let a = this.data.subarray(
-                index_a, index_a+this.columns)
-            let b = this.data.subarray(index_b, index_b+this.columns)
+        for(let i =0; i<index_pairs.length; i++){
+            let index_a = index_pairs[i]   * this.columns;
+            let index_b = index_pairs[i+1] * this.columns;
+            let a = this.data.subarray(index_a, index_a + this.columns)
+            let b = this.data.subarray(index_b, index_b + this.columns)
             for(let k=0; k < this.columns; k++){
                 store.data[result_index+k] = b[k]-a[k]
             }
@@ -87,11 +98,23 @@ class v32{
         }
         return store;
     }
+
     /**
     * @returns {v32} a copy of this v32 object refering to a new array in memory
     */
     clone(){
         return new v32(Float32Array.from(this.data), this.rows, this.columns);
+    }
+
+    /**
+    * @param {v32} other Copy values from other into self
+    * @returns {v32} self
+    */
+    copy_values(other){
+        for(let i =0;i<this.data.length;i++){
+            this.data[i] = other.data[i]
+        }
+        return this
     }
 
     // ========================================
@@ -108,6 +131,7 @@ class v32{
         }
         return this;
     }
+
     /**
      * sub other from self
      * @param {v32} other 
@@ -119,6 +143,7 @@ class v32{
         }
         return this;
     }
+
     /**
      * multiply other from self (elementwise)
      * @param {v32} other 
@@ -132,12 +157,75 @@ class v32{
     }
 
     /**
+     * divide self by other (elementwise) store in self
+     * @param {v32} other 
+     * @returns {v32} self
+     */
+    div_self(other){
+        for(let i = 0; i < this.data.length; i++){
+            this.data[i] /= other.data[i];
+        }
+        return this;
+    }
+
+    /**
+     * divide self by other column (broadcasting over columns) store in self
+     * @param {v32} other 
+     * @returns {v32} self
+     */
+     div_column_self(column){ // works best at 5_000_000 rows
+        for(let i = 0; i < column.data.length; i++){
+            let offset = i*this.columns;
+            for(let j=0;j<this.columns;j++){
+                this.data[offset + j] /= column.data[i];
+            }
+        }
+        return this;
+    }
+
+    mul_column_self(column){
+        for(let i = 0; i < column.data.length; i++){
+            let offset = i*this.columns;
+            for(let j=0;j<this.columns;j++){
+                this.data[offset + j] *= column.data[i];
+            }
+        }
+        return this;
+    }
+    /**
+     * @param {Number} scalar
+     * @returns {v32} self
+     */
+     scalar_mul_self(scalar){
+        scalar = Math.fround(scalar)
+        for(let i = 0;i<this.data.length;i++){
+            store.data[i] = this.data[i] * scalar;
+        }
+        return this;
+    }
+
+
+    invert_self(){ 
+        for(let i = 0; i < this.data.length; i++){
+            this.data[i] = Math.fround(1) / this.data[i];
+        }
+        return this;
+    }
+
+    replace_nan_self(){ 
+        for(let i = 0; i < this.data.length; i++){
+            this.data[i] = isNaN(this.data[i]) ? 0 : this.data[i];
+        }
+        return this;
+    }
+
+    /**
      * 
      * @param {v32} other
      * @param {v32} store
      * @returns {v32} store
      */
-    add_then_store(other, store){
+    add_store(other, store){
         for(let i = 0;i<this.data.length;i++){
             store.data[i] = this.data[i] + other.data[i];
         }
@@ -201,7 +289,7 @@ class v32{
      * @param {v32} other 
      * @param {Float32Array} store 
      */
-    dot_then_store(other, store){
+    dot_store(other, store){
         for(let i = 0; i<this.data.length; i++){
             store[i % row] += this.data[i] * other.data[i]
         }
@@ -238,4 +326,10 @@ class v32{
         console.log(this.toString())
     }
 
+
+    * iter_rows(){
+        for(let i=0;i<this.data.length;i+=this.columns){
+            yield this.data.subarray(i, i+this.columns);
+        }
+    }
 }
